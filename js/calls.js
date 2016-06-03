ec51dc75c252009f5764c83af52d06d1108cc8d9
ctevnan <carolyntevnan@yahoo.com>
2016-06-03 16:57:20 -0400

    building table row with databases

--- js/calls.js
+++ js/calls.js
@@ -1,3 +1,16 @@
 $(Document).ready(function(){
-  
+  $(".btn-default").on("click", "a", function(e){
+    e.preventDefault();
+    
+    $.ajax({
+      type: "GET",
+      url: $(this).attr("href"),
+      success: function(databases) {
+        $("tbody").empty();
+        for (var i = 0; i < databases.length; i++) {
+          $("tbody").append(buildTableRow(databases[i]))
+        }
+      }
+    })
+  }) 
 }
