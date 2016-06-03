c264d453f4bac8f17df4ceec1c053fd8a765977b
ctevnan <carolyntevnan@yahoo.com>
2016-06-03 17:18:37 -0400

    using another get call for photos

--- js/calls2.js
+++ js/calls2.js
@@ -1,6 +1,22 @@
-$(document).readdy(function(){
+$(document).ready(function(){
   $.ajax({
     type: "GET",
-    url: 
-  })
+    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=f6cd6592-f61e-40b2-afcc-c38827f075df",
+    success: function(databases) {
+      for (var i = 0; i < databases.length; i++) {
+        var newListItem = buildListGroup(databases[i]);
+        $(".list-group").append(newListItem);
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert("error");
+    }
+  });
+
+  function buildListGroup(photoData) {
+    var photoApiUrl = "https://matchapi.halberdtechnologies.com/api/Database?userID=f6cd6592-f61e-40b2-afcc-c38827f075df";
+
+    
+
+  }
 })
