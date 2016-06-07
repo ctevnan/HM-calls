94e850eb0e88e3f1ce22cb716f93694dadf0d8fa
ctevnan <carolyntevnan@yahoo.com>
2016-06-07 13:07:00 -0400

    checking out console.logs

--- js/calls2.js
+++ js/calls2.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     type: "GET",
     url: 'http://matchapi.halberdtechnologies.com/api/Database?userID=f6cd6592-f61e-40b2-afcc-c38827f075df',
     dataType: "jsonp",
-    success: function(images) {
-      console.log(images);
-     /* for (var i = 0; i < images.length; i++) {
-        var newListItem = buildListGroup(images[i]);
+    success: function(database) {
+      console.log(database);
+     /* for (var i = 0; i < database.length; i++) {
+        var newListItem = buildListGroup(database[i]);
         $(".list-group").append(newListItem);
       }
     },
@@ -19,20 +19,20 @@ $(document).ready(function(){
     type: "POST",
     dataType: "jsonp",
     url: "http://matchapi.halberdtechnologies.com/api/Database?userID=2bb2b13d-1663-4198-88e2-a8cel63e8a28&database=cad87234-911b-4d90-ba0e-7e3ecb97ca62",
-    success: function(database) {
-      console.log(database);
+    success: function(images) {
+      console.log(images);
     }
   })
   /*  function buildListGroup(images) {
       var halberdApiUrl = "http://matchapi.halberdtechnologies.com/api/Database?userID=f6cd6592-f61e-40b2-afcc-c38827f075df";
 
-      halberdApiUrl += photoData.owner.login + "/";
-      halberdApiUrl += photoData.name + "/photos";
+      halberdApiUrl += imageData.owner.login + "/";
+      halberdApiUrl += imageData.name + "/photos";
 
         var newLink = $("<a>")
           .attr("href", halberdApiUrl)
           .addClass("list-group-item")
-          .append(photoData);
+          .append(imageData);
         return newLink;
       } 
   }); 
