f8547a092d9691d4b1ffa2faa11e74327348468e
ctevnan <carolyntevnan@yahoo.com>
2016-06-07 12:57:21 -0400

    tried different header for get call, still status 500 internal server error. going back to jsonp strategy

--- js/calls2.js
+++ js/calls2.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
   $.ajax({
     type: "GET",
+    url: 'http://matchapi.halberdtechnologies.com/api/Database?userID=f6cd6592-f61e-40b2-afcc-c38827f075df',
     dataType: "jsonp",
-    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=f6cd6592-f61e-40b2-afcc-c38827f075df",
-    success: function(database) {
-      console.log(database);
-      for (var i = 0; i < databases.length; i++) {
-        var newListItem = buildListGroup(databases[i]);
+    success: function(images) {
+      console.log(images);
+     /* for (var i = 0; i < images.length; i++) {
+        var newListItem = buildListGroup(images[i]);
         $(".list-group").append(newListItem);
       }
     },
     error: function(jqXHR, textStatus, errorThrown) {
-      alert("error");
+      alert("error");*/
     }
   });
   
   $.ajax({
     type: "POST",
-    dataType: "json",
+    dataType: "jsonp",
     url: "http://matchapi.halberdtechnologies.com/api/Database?userID=2bb2b13d-1663-4198-88e2-a8cel63e8a28&database=cad87234-911b-4d90-ba0e-7e3ecb97ca62",
     success: function(database) {
       console.log(database);
