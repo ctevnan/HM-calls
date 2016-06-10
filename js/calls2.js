3efee612e0cfe62fe7d419ba7811f5225e03e489
ctevnan <carolyntevnan@yahoo.com>
2016-06-10 18:14:14 -0400

    both calls are working with one error in console- unexpected token : . if I try to switch data type from jsonp to json, the error goes away and is replaced with a new error- no xml header present

--- js/calls2.js
+++ js/calls2.js
@@ -1,25 +1,17 @@
 $(document).ready(function(){
-  /*$(".list-group").on("click", "a", function(e) {
-    e.preventDefault(); */
+
   $.ajax({
     type: "GET",
     url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder",
-    /*response: "Access-Control-Allow-Origin", true*/
     dataType: "jsonp",
     success: function(response) {
       console.log(response);
       }
-    })
-    /*error: function (jqXHR, textStatus, errorThrown) {
-      alert("errorThrown");
-        console.log(errorThrown);
-      })*/
-/*}) */   
+    }) 
 
-    /*$.ajax({
-    type: "POST",*/
-   /* Response: "Access-Control-Allow-Origin", true
-    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder2&database=TestFolder2&jsonp=?",
+    $.ajax({
+    type: "POST",
+    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder&database=TestFolder",
     dataType: "jsonp",
     success: function(response) {
       console.log(response); 
@@ -28,18 +20,5 @@ $(document).ready(function(){
       alert("errorThrown");
         console.log(errorThrown);
       }
-  });*/
+  });
 });   
-    
-   /* function buildListGroup(images) {
-      var halberdApiUrl = "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder";
-      halberdApiUrl += TestFolder2.TestFolder + "/";
-      halberdApiUrl += images.name + "/images";
-
-      var newLink = $("<a>")
-        .attr("href", halberdApiUrl)
-        .addClass("list-group-item")
-        .append(imageData);
-          return newLink;
-        }  
-      }) */
