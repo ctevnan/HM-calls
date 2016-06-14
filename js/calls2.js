7b67bedb4f569ef00a6e5c254c62dc18637513b8
ctevnan <carolyntevnan@yahoo.com>
2016-06-14 11:22:41 -0400

    committing changes to api calls. a post cannot work with jsonb

--- js/calls2.js
+++ js/calls2.js
@@ -4,21 +4,23 @@ $(document).ready(function(){
     type: "GET",
     url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder",
     dataType: "jsonp",
-    success: function(data) {
+    success: function(data, textStatus, jqXHR) {
       console.log(data);
+      console.log(textStatus);
+      console.log(jqXHR);
       }
     }) 
-
+    
+    /*//jsonp will not work for posts
     $.ajax({
     type: "POST",
     url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder&database=TestFolder",
-    dataType: "jsonp",
+    dataType: "json",
     success: function(data) {
       console.log(data); 
     },
     error: function(jqXHR, textStatus, errorThrown) {
       alert("errorThrown");
         console.log(errorThrown);
-      }
-  });
-});      
+      }*/
+  });      
