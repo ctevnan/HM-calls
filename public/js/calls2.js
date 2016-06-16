6ff2224d1addfc6f5409cdbe0ee32a05ef1b59de
ctevnan <carolyntevnan@yahoo.com>
2016-06-16 13:07:18 -0400

    trying a call without specifying datatype

--- public/js/calls2.js
+++ public/js/calls2.js
@@ -1,26 +1,34 @@
 $(document).ready(function(){
-  //making the call this way to see if server supports jsonp
+
   $.ajax({
     type: "GET",
-    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder&callback=myFunction",
-    dataType: "jsonp",
-    success: function(data, textStatus, jqXHR) {
-      console.log(data);
-      console.log(textStatus);
-      console.log(jqXHR);
-      }
-    }) 
+    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder",
+    success: function(response) {
+      
+      successcallback(response);
+    } 
   });
+  function successCallback(responseObj){
+    //read response, show data
+    alert(JSON.stringify(responseObj)); //only applicable to JSON response
+  }
+  
 
-    /*$.ajax({
+  $.ajax({
     type: "POST",
     url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder&database=TestFolder",
-    dataType: "jsonp",
-    success: function(data) {
-      console.log(data); 
+    success: function(response) {
+      console.log(response); 
     },
     error: function(jqXHR, textStatus, errorThrown) {
       alert("errorThrown");
         console.log(errorThrown);
-      }*/
+      }
+    }
+    function successCallback(responseObj){
+      alert(JSON.stringify(responseObj));
+    }
+  })
+});    
+      
       
