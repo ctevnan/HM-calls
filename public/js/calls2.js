9d05cd09387aed5204d8c1fe7d5568b8acb4a066
ctevnan <carolyntevnan@yahoo.com>
2016-07-07 13:42:45 -0400

    changes to ui, ajax . tested endpoint to see if they are cors enabled. test returned with xhr status of 0, so responses from the server may be blocked by firewall

--- public/js/calls2.js
+++ public/js/calls2.js
@@ -1,43 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  function createCORSRequest(method, url){
+  function createCORSRequest(method, url) {
     var xhr = new XMLHttpRequest();
-      xhr.open('GET','http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder', true);
+      xhr.open('GET', 'http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder', true);
       xhr.setRequestHeader('Access-Control-Allow-Origin', true);
       xhr.send();
-    }
+    }  
   });
 
   $.ajax({
     type:"GET",
     url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder",
-    success: function(data, textStatus, jqHXR) {
-      console.log(data);
-      console.log(textStatus);
-      console.log(jqHXR);
-    },
-    error: function(data, textStatus, jqXHR) {
+    success: function(data, textStatus, jqXHR) {
       console.log(data);
       console.log(textStatus);
       console.log(jqXHR);
     }
-  })
-});  
+  })  
     
-    /*$.ajax({
-      type: "GET",
-      url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder",
-      success: function(userID) {
-        for (var i = 0; i < userID.length; i++) {
-          var newListItem = buildListGroup(userID[i]);
-          $(".list-group").append(newListItem)
-        }
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        alert("error");
-      } 
-    });*/
-
   $.ajax({
     type: "POST",
     url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder&database=TestFolder",
@@ -47,12 +27,9 @@ $(document).ready(function(){
       console.log(jqXHR);
     },
     error: function(jqXHR, textStatus, errorThrown) {
-      alert("errorThrown");
-      console.log(errorThrown);
-      }
-    })
-    function successCallback(responseObj){
-      alert(JSON.stringify(responseObj));
+      console.log(data);
+      console.log(textStatus);
+      console.log(jqXHR);
     }
   });
 });
