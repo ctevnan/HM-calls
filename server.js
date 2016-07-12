10f3808e409866b3548b5e97b7649cd198208324
ctevnan <carolyntevnan@yahoo.com>
2016-07-12 14:56:52 -0400

    adding npm package multer for handling multipart/form data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 var app = express();
 var morgan = require('morgan');
 var http = require('http');
