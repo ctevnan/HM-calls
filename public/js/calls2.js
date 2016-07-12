491e415b26a0d834b8e08bca9ffafc05d5b6048a
ctevnan <carolyntevnan@yahoo.com>
2016-07-12 13:22:44 -0400

    changing cb for get request

--- public/js/calls2.js
+++ public/js/calls2.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
   $.ajax({
     type:"GET",
     url: "http://matchapi.halberdtechnologies.com/api/Database?userID=13e3961f-b35c-41b7-bc49-b0557e7c6d62",
-    success: function(verified) {
-      alert('verified');
+    success: function(currentDatabase) {
+      alert('currentDatabase');
     }
   });
     
