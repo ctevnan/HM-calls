bb9b06416fa486a2fc77df3baa039de1352ff2fc
ctevnan <carolyntevnan@yahoo.com>
2016-07-13 15:46:54 -0400

    adding/updating body-parsing middleware

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var upload = multer(); //parses multipart/ form-data
 var app = express();
 var morgan = require('morgan');
 var http = require('http');
 var PORT = process.env.PORT || 80;
 var server = require('node-http-server');
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json()); //parses application/json
+app.use(bodyParser.urlencoded({ extended: true })); //parses application/x-www-form-urlencoded
 
-app.use(function(req, res, next) {
+app.post('/profile', upload.array(), function (req, res, next) {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.use(function (req, res, next)) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
