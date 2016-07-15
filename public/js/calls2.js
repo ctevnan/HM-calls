8abdb7617559b4a5ec9546b0fbe870ff73a5c031
ctevnan <carolyntevnan@yahoo.com>
2016-07-15 16:15:26 -0400

    trying out output=json and callback=function

--- public/js/calls2.js
+++ public/js/calls2.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   $.ajax({
     type:"GET",
-    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=13e3961f-b35c-41b7-bc49-b0557e7c6d62",
+    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=13e3961f-b35c-41b7-bc49-b0557e7c6d62&output=json&callback=ws_results",
     success: function(response) {
       console.log(response);
     }
   });
 
-  $.ajax({
-    type:"POST",
-    url: "http://matchapi.halberdtechnologies.com/api/FileUpload?userID=13e3961f-b35c-41b7-bc49-b0557e7c6d62&currentDB=Verify&mode=todatabase",
-    success: function(response) {
-      console.log(response);
-    }
-  });
+  // $.ajax({
+  //   type:"POST",
+  //   url: "http://matchapi.halberdtechnologies.com/api/",
+  //   success: function(response) {
+  //     console.log(response);
+  //   }
+  // });
 });    
