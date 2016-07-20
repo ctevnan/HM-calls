3d985da6ca89f374a1730ecfc1b95d9b07cb459a
ctevnan <carolyntevnan@yahoo.com>
2016-07-20 12:03:15 -0400

    changes to router to include json and form

--- router.js
+++ router.js
@@ -1,5 +1,34 @@
+var http = require("http");
 var express = require('express');
-var router = express.Router();
+var Router = require("routes-router");
+var jsonBody = require("body/json");
+var formBody = require("body/form");
+//use body with generators. you must pass a cb
+//async turns a generator into an async function taking a cb
+var async= require("gens");
+
+var app = Router({
+  //add call. jsonBody err goes here
+  errorHandler: function (req, res, err) {
+    res.statusCode = 500
+    res.end(err.message)
+  }
+})
+
+app.addRoute("/json", async(function* (req, res) {
+  //if jsonBody has an error it goes to the cb
+  var body = yield jsonBody(req, res)
+
+  res.setHeader("content-type", "application/json")
+  res.end(Json.stringify(body))
+}))
+
+app.addRoute("/form", async(function* (req, res) {
+  var body = yield formBody(req, res)
+
+  res.setHeader("content-type", "application/json")
+  res.end(JSON.stringify(body))
+}))
 
 /* get homepage */
 router.get('/', function(req, res) {
