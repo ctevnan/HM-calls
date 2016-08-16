9a2205ce2c1902af39ffb412f8616318d0dd510e
ctevnan <carolyntevnan@yahoo.com>
2016-08-16 12:26:06 -0400

    adding post

--- public/js/calls2.js
+++ public/js/calls2.js
@@ -1,28 +1,31 @@
 $(document).ready(function() {
-  $(".btn btn-default").on("click", function(e) {
-    e.preventDefault();
-    
-    var method = $("method").val();
-    var userID = $("userID").val();
-    var currentDB = $("currentDB").val();
-    var mode = $("mode").val();
-    var halberdApiUrl = "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder";
 
-    halberdApiUrl += "&userID" + userID + "&currentDB" + currentDB + "&mode" + mode;
-
-    $.ajax({
-      type:"GET",
-      url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder",
-      success: function(data, textStatus, jqXHR) {
-        console.log(data);
-        console.log(textStatus);
-        console.log(jqXHR);
-      },
-      error: function(data,textStatus, jqXHR) {
-        console.log(data);
-        console.log(textStatus);
-        console.log(jqXHR);
-      }
-    });
+  $.ajax({
+    type:"GET",
+    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder",
+    success: function(data, textStatus, jqXHR) {
+      console.log(data);
+      console.log(textStatus);
+      console.log(jqXHR);
+    },
+    error: function(data,textStatus, jqXHR) {
+      console.log(data);
+      console.log(textStatus);
+      console.log(jqXHR);
+    }
+  });
+  $.ajax({
+    type: "POST",
+    url: "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder&database=VPNVerify888",
+    success: function(data, textStatus, jqXHR) {
+      console.log(data);
+      console.log(textStatus);
+      console.log(jqXHR);
+    },
+    error: function(data, textStatus, jqHXR) {
+      console.log(data);
+      console.log(textStatus);
+      console.log(jqXHR);
+    }
   });
 });   
