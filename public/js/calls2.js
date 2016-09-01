02b6d8727f80435826c61d116b052c863c1c4310
ctevnan <carolyntevnan@yahoo.com>
2016-09-01 15:58:17 -0400

    finally solved blocking error, xmlhttprequest cannot load request uri no access control allow origin header is present on the requested resource by adding addtl info to request uri. now all xhr has finished loading an objects are at readystate 4, status 200 ok

--- public/js/calls2.js
+++ public/js/calls2.js
@@ -1,18 +1,21 @@
 $(document).ready(function () {
+ 
+ var url = 'https://cors-anywhere.herokuapp.com/http://matchapi.halberdtechnologies.com/api/Database?userID=testFolder';
 
- $.getJSON("http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder", function(json){
+ $.ajax({
+  url: url,
+  complete: function(data){
+    console.log(data);
+  }
+ });
+ 
+ $.getJSON("https://cors-anywhere.herokuapp.com/http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder", function(json){
     console.log(json);
   });
 
- /* var json = '{ "name":"6712c44f-9d04-498b-ac68-c1538ad72408_-10_-1_.bmp", "userID":"testFolder","currentDatabase":"VPNVerify888" }';
-  var image = JSON.parse(json);
-  alert(image.name + " " + image.testFolder);*/
-
-  
-
  $.ajax({
   type: "GET",
-  url: "http://matchapi.halberdtechnologies.com/api/Database?userID=testFolder&output=json",
+  url: "https://cors-anywhere.herokuapp.com/http://matchapi.halberdtechnologies.com/api/Database?userID=testFolder&output=json",
   success: function(data, textStatus, jqXHR) {
     console.log(data);
     console.log(textStatus);
