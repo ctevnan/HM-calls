1a1e023e182ddaa1c2080d0d4bcfafff021fa736
ctevnan <carolyntevnan@yahoo.com>
2016-09-01 14:32:02 -0400

    removing html lang = en and meta charset = utf-8

--- public/js/calls2.js
+++ public/js/calls2.js
@@ -4,6 +4,12 @@ $(document).ready(function () {
     console.log(json);
   });
 
+  var json = '{ "name":"6712c44f-9d04-498b-ac68-c1538ad72408_-10_-1_.bmp", "userID":"testFolder","currentDatabase":"VPNVerify888" }';
+  var image = JSON.parse(json);
+  alert(image.name + " " + image.testFolder);
+
+  
+
  $.ajax({
   type: "GET",
   url: "http://matchapi.halberdtechnologies.com/api/Database?userID=testFolder&output=json",
