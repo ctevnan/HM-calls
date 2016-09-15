6ea26dba89499b2f08c119927f575b2385c0a853
ctevnan <carolyntevnan@yahoo.com>
2016-09-15 12:26:28 -0400

    adding post

--- public/js/calls2.js
+++ public/js/calls2.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   var a1 = [{"name":"6712c44f-9d04-498b-ac68-c1538ad72408_-10_-1_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"6712c44f-9d04-498b-ac68-c1538ad72408_-20_-2_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"6712c44f-9d04-498b-ac68-c1538ad72408_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"6712c44f-9d04-498b-ac68-c1538ad72408_10_1_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"6712c44f-9d04-498b-ac68-c1538ad72408_20_2_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8699238a-3d32-47e4-a314-1c780aa3b59f_-10_-1_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8699238a-3d32-47e4-a314-1c780aa3b59f_-20_-2_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8699238a-3d32-47e4-a314-1c780aa3b59f_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8699238a-3d32-47e4-a314-1c780aa3b59f_10_1_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8699238a-3d32-47e4-a314-1c780aa3b59f_20_2_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8f315f49-b42a-4da0-9361-dd03dd61c6f4_-10_-1_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8f315f49-b42a-4da0-9361-dd03dd61c6f4_-20_-2_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8f315f49-b42a-4da0-9361-dd03dd61c6f4_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8f315f49-b42a-4da0-9361-dd03dd61c6f4_10_1_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"8f315f49-b42a-4da0-9361-dd03dd61c6f4_20_2_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"b0384360-50da-4ff5-b31c-ccf05486dd80_-10_-1_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"b0384360-50da-4ff5-b31c-ccf05486dd80_-20_-2_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"b0384360-50da-4ff5-b31c-ccf05486dd80_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"b0384360-50da-4ff5-b31c-ccf05486dd80_10_1_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"},{"name":"b0384360-50da-4ff5-b31c-ccf05486dd80_20_2_.bmp","userID":"testFolder","currentDatabase":"VPNVerify888"}];
   getData();
+  postData();
 
   function getData(){
     $.ajax({
@@ -21,5 +22,21 @@ $(document).ready(function () {
         });
       }
     });
-  }   
+  }
+  function postData(){
+    $.ajax({
+      url:"https://cors-anywhere.herokuapp.com/http://matchapi.halberdtechnologies.com/api/Database?userID=testFolderTwo&database=VPNVerify888",
+      method:"POST",
+      success: function(data, textStatus, jqXHR){
+        console.log(data);
+        console.log(textStatus);
+        console.log(jqXHR);
+      },
+      error: function(data, textStatus, jqXHR){
+        console.log(data);
+        console.log(textStatus);
+        console.log(jqXHR);
+      } 
+    });
+  }    
 });  
