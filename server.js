59c0510fb3ebb6b6347b11f70434689b467db96b
ctevnan <carolyntevnan@yahoo.com>
2016-09-16 13:53:12 -0400

    adding form

--- server.js
+++ server.js
@@ -7,10 +7,32 @@ var http = require("http");
 var util = require("util");
 
 var PORT = process.env.PORT || 3000;
-/*
-http.createServer(function(req, res{
-  if (req.url == '/upload' && )
-}))*/
+
+app.post('/upload', function(req, res, next){
+  res.send(req.files.file.path);
+});
+http.createServer(function(req, res) {
+  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+    //parse file upload
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));
+    });
+    return;
+  }
+  //show file upload form
+  res.writeHead(200, {'content-type':'text/html'});
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+}).listen(3000, '127.0.0.1');
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
